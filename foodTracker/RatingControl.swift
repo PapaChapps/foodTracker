d57fd427487f195019882ce67ff1bfb42a6dd8fd
PapaChapps <Makaychapp@gmail.com>
2018-08-30 09:55:22 -0600

    first

---
+++ foodTracker/RatingControl.swift
@@ -0,0 +1,34 @@
+//
+//  RatingControl.swift
+//  foodTracker
+//
+//  Created by Makay Chapple on 8/30/18.
+//  Copyright © 2018 Makay Chapple. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    // MARK: Initilization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder ){
+            super.init(coder: coder)
+    }
+            
+    //MARK: Private Meathods
+    private func  setupButtons() {
+    //create the button
+        let button = UIButton()
+        button.backroundColor = UIColor.red
+        
+
+
+
+        
+
+}
--- foodTracker/ViewController.swift
+++ foodTracker/ViewController.swift
@@ -8,18 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+   
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField! //Make sure to connect these events ir swift wont know
+    @IBOutlet weak var mealNameLabel: UILabel!  // every time if its a nil crash its because you didnt connect these
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
-
+    
+    // do any additional setup from the view typically from a nib
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing( _ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // set photoImageView to display the selected image.
+        
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
 
+    
+    //MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: Any) { //enable user interaction
+        // hide the keyoard
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
 
+    @IBAction func setDefaultLabelText(_sender: UIButton) {
+        mealNameLabel.text = "Default Label Text"
+    }
 }
+
+
+
+
+
+
+
 
